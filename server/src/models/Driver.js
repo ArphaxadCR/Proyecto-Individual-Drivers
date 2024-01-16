55ea9ea8d44ad417f29ff70bfc175043c3755db3
ArphaxadCR <arphaxad.calderon@gmail.com>
2024-01-15 19:48:01 -0600

    Generacion de modelos y relaciones

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -1,12 +1,42 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Driver', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Driver",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      apellido: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      descripcion: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      imagen: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nacionalidad: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      fechaNacimiento: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
