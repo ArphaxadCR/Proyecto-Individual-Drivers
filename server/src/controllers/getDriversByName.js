757b323f3135315552c431e8163552249c4587e9
ArphaxadCR <arphaxad.calderon@gmail.com>
2024-01-17 16:52:19 -0600

    Implementacion controller getDriversByName

--- server/src/controllers/getDriversByName.js
+++ server/src/controllers/getDriversByName.js
@@ -1,8 +1,34 @@
 const axios = require("axios");
+const { Driver } = require("../db");
+const { Op } = require("sequelize");
 
 async function getDriversByName(req, res) {
   try {
-    res.send("La ruta de getDriversByName está funcionando");
+    const { name } = req.query;
+
+    let [primeraLetra, ...resto] = name;
+
+    const formattedName =
+      primeraLetra.toUpperCase() + resto.join("").toLowerCase();
+
+    const [apiResponse, bdResponse] = await Promise.all([
+      axios.get(`http://localhost:5000/drivers?name.forename=${formattedName}`),
+      Driver.findAll({
+        where: {
+          forename: {
+            [Op.like]: `%${formattedName}%`,
+          },
+        },
+      }),
+    ]);
+
+    const finalResponse = [...apiResponse.data, ...bdResponse];
+
+    if (finalResponse.length === 0) {
+      res.status(404).send("Drivers not founded");
+    } else {
+      res.status(200).json(finalResponse);
+    }
   } catch (error) {
     res.status(500).send(error);
   }
