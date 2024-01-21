550e14dd50c5ddde709de0c08768d7796ccea33a
ArphaxadCR <arphaxad.calderon@gmail.com>
2024-01-20 19:51:01 -0600

    Implementacion de cards y card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,11 +1,45 @@
+/* eslint-disable react/prop-types */
 import style from "./Card.module.css";
+// import { Link } from "react-router-dom";
+import imagenPorDefecto from "../../assets/imagenPorDefecto.jpg";
 import { Link } from "react-router-dom";
 
-function Card() {
+function Card(props) {
+  const idRegex1 =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+  console.log(props);
   return (
     <div className={style.card}>
-      <p>Esto es una card</p>
-      <Link to={"/detail/1"}>Ir a detail</Link>
+      {!idRegex1.test(props.driver.id) ? (
+        <>
+          <Link to={`/detail/${props.driver.id}`}>
+            <img
+              className={style.img}
+              src={props.driver.image?.url || imagenPorDefecto}
+              alt=""
+            />
+          </Link>
+          <h2 className={style.name}>{props.driver.driverRef}</h2>
+          <p className={style.paragraph}>{props.driver.teams}</p>
+        </>
+      ) : (
+        <>
+          <Link to={`/detail/${props.driver.id}`}>
+            <img
+              className={style.img}
+              src={
+                props.driver.image && props.driver.image.url
+                  ? props.driver.image.url
+                  : imagenPorDefecto
+              }
+              alt="Imag"
+            />
+          </Link>
+          <h2 className={style.name}>{props.driver.forename}</h2>
+          <h3>{props.driver.surname}</h3>
+        </>
+      )}
     </div>
   );
 }
--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,11 +1,24 @@
 import style from "./Cards.module.css";
 import Card from "../Card/Card";
+
+import { getDrivers } from "../../redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+
+import { useEffect } from "react";
+
 function Cards() {
+  const dispatch = useDispatch();
+  const drivers = useSelector((state) => state.drivers);
+
+  useEffect(() => {
+    dispatch(getDrivers());
+  }, [dispatch]);
+
   return (
     <div className={style.cards}>
-      <Card />
-      <Card />
-      <Card />
+      {drivers.map((driver) => (
+        <Card key={driver.id} driver={driver} />
+      ))}
     </div>
   );
 }
